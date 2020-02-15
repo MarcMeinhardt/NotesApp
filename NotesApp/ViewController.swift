254ef20eb4886ef746709a0c6ebee8ea895857a5
marcmeinhardt <meinhardtmarc@outlook.com>
2020-02-15 17:39:21 +0100

    Initial Commit

---
+++ NotesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by Marc Meinhardt on 15.02.20.
+//  Copyright © 2020 Marc Meinhardt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
